44adfb351478e69ed4377206ad5aeb458d34a369
Parth Patel <parth05211@gmail.com>
2023-12-10 23:22:03 -0500

    Added test.rest (REST client)

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+
+let people = [
+    {id:1, name:'parth'}
+];
+
+app.get("/people",(req,res)=>{
+    res.status(200).json(people);
+})
 // listen to the external requests on this port.
 app.listen(PORT,()=>{
     console.log(`server has started on port ${PORT}`)
